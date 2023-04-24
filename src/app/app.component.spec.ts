8a38ce6f4fc39f78db0b192ab1b0b49ad04ea41c
Anmol <anmolshingari2000@gmail,com>
2023-04-24 17:53:21 +0530

    milestone

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'job4'`, () => {
+  it(`should have as title 'jobportal1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('job4');
+    expect(app.title).toEqual('jobportal1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('job4 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('jobportal1 app is running!');
   });
 });
